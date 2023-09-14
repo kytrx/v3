1b960dbd91da8a46e8c579d354dd03dd4b09d27e
kytrx <141412426+kytrx@users.noreply.github.com>
2023-09-15 05:04:34 +0700

    Update installsl.sh

--- slowdns/installsl.sh
+++ slowdns/installsl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+REPO="https://raw.githubusercontent.com/kytrx/v3/main/"
 ns_domain_cloudflare() {
 	DOMAIN=sloowdns.my.id
 	DOMAIN_PATH=$(cat /etc/xray/domain)
@@ -47,15 +48,15 @@ ns_domain_cloudflare() {
 setup_dnstt() {
 	cd
 	mkdir -p /etc/slowdns
-	wget -O dnstt-server "https://wokszxd.xcodehoster.com/wireguard/dnstt-server" >/dev/null 2>&1
+	wget -O dnstt-server "${REPO}slowdns/dnstt-server" >/dev/null 2>&1
 	chmod +x dnstt-server >/dev/null 2>&1
-	wget -O dnstt-client "https://wokszxd.xcodehoster.com/wireguard/dnstt-client" >/dev/null 2>&1
+	wget -O dnstt-client "${REPO}slowdns/dnstt-client" >/dev/null 2>&1
 	chmod +x dnstt-client >/dev/null 2>&1
 	./dnstt-server -gen-key -privkey-file server.key -pubkey-file server.pub
 	chmod +x *
 	mv * /etc/slowdns
-	wget -O /etc/systemd/system/client.service "https://wokszxd.xcodehoster.com/wireguard/client" >/dev/null l 2>&1
-	wget -O /etc/systemd/system/server.service "https://wokszxd.xcodehoster.com/wireguard/server" >/dev/null l 2>&1
+	wget -O /etc/systemd/system/client.service "${REPO}slowdns/client" >/dev/null l 2>&1
+	wget -O /etc/systemd/system/server.service "${REPO}slowdns/server" >/dev/null l 2>&1
 	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/client.service 
 	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/server.service 
 }
